f59f749dd06bb07bdde4c7415966ddde54fc13c8
mbusenitz <mbusenitz@gmail.com>
2017-02-05 02:57:06 -0800

    test

--- cstar.js
+++ cstar.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
+'use strict';
+const compose = require( './node_modules/filebase/compose' )
 
-const compose = require( './node_modules/filebase/compose' );
+function makeGYP( defPath ) {
+  return new Promise( (resolve, reject) => {
+    compose( defPath )
+    .then( result => {
+      let gypi = { target_defaults: {} };
 
-compose( './def.json' )
-.then( result => {
-	var gypi = {
-		target_defaults: {}
-	};
+      if (result.hasOwnProperty('config')) {
+        gypi.includes = Array.isArray(result.config) ? result.config : [result.config];
+      }
 
-	if (result.hasOwnProperty('config')) {
-		gypi.includes = Array.isArray(result.config) ? result.config : [result.config];
-	}
+      if (result.hasOwnProperty('sources')) {
+        gypi.target_defaults.sources = Array.isArray(result.sources) ? result.sources : [result.sources];
+      }
+      resolve( gypi );
+    });
+  });
+}
 
-	if (result.hasOwnProperty('sources')) {
-		gypi.target_defaults.sources = Array.isArray(result.sources) ? result.sources : [result.sources];
-	}
-
-	console.log( JSON.stringify( gypi, null, 2 ) );
-});
+module.exports = makeGYP;
--- test/test.js
+++ test/test.js
@@ -2,30 +2,17 @@
 
 'use strict';
 
-const compose = require( '../node_modules/filebase/compose' )
+const cstar = require( '../cstar' )
   , Expector = require( 'expector' ).SeqExpector
   , test = require( 'tape' )
 
 test( 'compose', t => {
-
 	let e = new Expector( t );
 
 	e.expect( { target_defaults:{ sources: ["src/main.cpp"]}, includes: ["targets.gypi"]} );
 
-	compose( './def.json' )
-	.then( result => {
-		var gypi = {
-			target_defaults: {}
-		};
-
-		if (result.hasOwnProperty('config')) {
-			gypi.includes = Array.isArray(result.config) ? result.config : [result.config];
-		}
-
-		if (result.hasOwnProperty('sources')) {
-			gypi.target_defaults.sources = Array.isArray(result.sources) ? result.sources : [result.sources];
-		}
-
+	cstar( './def.json' )
+	.then( gypi => {
 		e.emit( gypi ).check();
 	});
 });
