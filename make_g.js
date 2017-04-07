bbe4a9949470628f5593c757e85a7c5684ac3afd
mbusenitz <mbusenitz@gmail.com>
2017-04-06 19:07:55 -0700

    fix

--- make_g.js
+++ make_g.js
@@ -4,7 +4,7 @@
 
 const run = require( 'runjs' ).run;
 
-run( 'cstar.js -g gyp lib.json > lib.gypi' );
+run( 'cstar -g gyp lib.json > lib.gypi' );
 run( 'gyp --depth=. --build=Test --format=make test.gyp' ); 
 run( 'make' ); 
 
--- make_q.js
+++ make_q.js
@@ -4,7 +4,7 @@
 
 const run = require( 'runjs' ).run;
 
-run( 'cstar.js -q qmake lib.json > lib.pri' );
+run( 'cstar -q qmake lib.json > lib.pri' );
 run( 'qmake test.pro' ); 
 run( 'make' ); 
 
