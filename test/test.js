6dc0b65bbd784e7e4c941950a721ce2d46805301
mbusenitz <mbusenitz@gmail.com>
2017-04-10 11:52:41 -0700

    hooked up opengl example

--- test/test.js
+++ test/test.js
@@ -58,12 +58,12 @@ test( 'compose cmake', t => {
 	});
 });
 
-test( 'make gyp include', t => { 
+test( 'make gyp example', t => { 
 
-	process.chdir( path.join( __dirname, 'cstar-template-gyp/' ) );
+	process.chdir( path.join( __dirname, 'cstar-example-gyp/' ) );
 
 	cstar.makeGYP( './def.json' )
-	.then( (gyp) => {
+	.then( gyp => {
 		fs.writeFile( './test.gypi', gyp, (err) => {
 			if (err) throw err;
 			cp.exec( 'gyp --depth=. ./host.gyp --format=make', (err, stdout, stderr) => {
@@ -84,12 +84,12 @@ test( 'make gyp include', t => {
 	.catch( t.fail.bind( t ) );
 });
 
-test( 'make qmake include', t => { 
+test( 'make qmake example', t => { 
 	
-	process.chdir( path.join( __dirname, 'cstar-template-qmake/' ) );
+	process.chdir( path.join( __dirname, 'cstar-example-qmake/' ) );
 	
 	cstar.makePRI( './def.json' )
-	.then( (pro) => {
+	.then( pro => {
 		fs.writeFile( './test.pri', pro, (err) => {
 			if (err) throw err;
 			cp.exec( 'qmake ./host.pro', (err, stdout, stderr) => {
@@ -105,12 +105,12 @@ test( 'make qmake include', t => {
 	.catch( t.fail.bind( t ) );
 });
 
-test( 'make CMake include', t => { 
+test( 'make CMake example', t => { 
 
-	process.chdir( path.join( __dirname, 'cstar-template-cmake/' ) );
+	process.chdir( path.join( __dirname, 'cstar-example-cmake/' ) );
 
 	cstar.makeCMake( './def.json' )
-	.then( (cmake) => {
+	.then( cmake => {
 		fs.writeFile( './test.txt', cmake, (err) => {
 			if (err) throw err;
 			cp.exec( 'cmake .', (err, stdout, stderr) => {
@@ -127,6 +127,30 @@ test( 'make CMake include', t => {
 	.catch( t.fail.bind( t ) );
 });
 
+test( 'make opengl example', t => {
 
+	process.chdir( path.join( __dirname, 'cstar-example-opengl/' ) );
+
+	cstar.makeGYP( './cstar-example-opengl.json' )
+	.then( gyp => {
+		fs.writeFile( './cstar-example-opengl.gypi', gyp, (err) => {
+			if (err) throw err;
+			cp.exec( 'gyp --depth=. ./test.gyp --format=make', (err, stdout, stderr) => {
+				if (err) throw err;
+				t.equal( stderr.length, 0 );
+				cp.exec( 'make test', (err, stdout, stderr) => {
+					
+					console.log( stderr );
+					console.log( stdout );
+
+					if (err) throw err;
+					t.equal( stderr.length, 0 );
+					t.end();
+				});
+			});
+		});
+	})
+	.catch( t.fail.bind( t ) );
+}); 
 
 
